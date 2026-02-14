c3803f441802b354d5d27d757e8166108e554743
pavi <pavishanan.surenthiran@gmail.com>
2026-02-14 13:19:55 -0500

    hi

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,26 @@ import 'react-pdf/dist/Page/TextLayer.css';
 {/* REQUIREMENT: GLOBAL WORKER */ }
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
+const downloadImage = async () => {
+    const templateUrl = '/template/TEMPLATE_v2.pdf';  // Note: starts with /
+    const response = await fetch(templateUrl);
+    const blob = await response.blob();
+
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'TEMPLATE_v2.pdf';
+    link.click();
+
+    URL.revokeObjectURL(url);
+}
+
 export default function Page() {
     return (
         <main data-section="whole page" className="bg-white text-black h-screen">
             <section data-section="fun buttons" className="p-8">
                 <section className="flex gap-8">
-                   <div>hi</div>
+                    <button>hi</button>
                 </section>
             </section>
         </main>
